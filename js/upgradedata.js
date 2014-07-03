a0f45f131e35e82d6a83cf05d41750bddbe8adba
idunnowhy9000 <idunnowhy9000@gmail.com>
2014-07-03 11:39:07 +0700

    Upgrades

---
+++ js/upgradedata.js
@@ -0,0 +1,121 @@
+var upgrades = {
+	// incandescent bulbs
+	"incandescentcy":{
+			amount:0,
+			name:"Thomas Edison",
+			cost:0,
+			boost:["incandescent",0.1]
+		},
+	"blueprints":{amount:0,
+			name:"Blueprints!",
+			cost:0,
+			boost:["incandescent",0.1]
+		},
+	"inventor":{
+		amount:0,
+		name:"Albert Einstein Clone",
+		cost:0,
+		desc:"Hire a clone and he'll (probably) help you out!",
+		boost:["incandescent",0.1]
+	},
+	// bulb smasher
+	"experimentroom":{amount:0,
+			name:"Experimentation Room",
+			cost:0,
+			boost:["smasher",0.1]
+		},
+	"bigham":{ // it's not what it sound like
+			name:"Big Hammer",
+			cost:0,
+			boost:["smasher",0.1],
+			amount:0
+		},
+	// thundercollector
+	"relaxation":{amount:0,
+			name:"Electron Relaxation",
+			cost:0,
+			boost:["thundercol",0.1],
+		},
+	"excitation":{amount:0,
+			name:"Electron Excitation",
+			desc:"The ultimate Electric Party!",
+			cost:0,
+			boost:["thundercol",0.1],
+		},
+	// bacterial
+	"fungus":{amount:0,
+			name:"Fungus",
+			desc:"I have to say, I'm a fungi!",
+			cost:0,
+			boost:["bacterial",0.1]
+		},
+	// dinosaur
+	"megadinos":{amount:0,
+			name:"Mega DinoBulbs",
+			cost:0,
+			boost:["dinosaur",0.1],
+			desc:"Still better than keys"
+		},
+	// human
+	"evo":{amount:0,
+			name:"EVOLUTION!",
+			cost:0,
+			boost:["human",0.1],
+			desc:"CREATIONISM!"
+		},
+	"creation":{amount:0,
+			name:"CREATIONISM!",
+			cost:0,
+			boost:["human",0.1],
+			desc:"EVOLUTION!"
+		},
+	"woodtools":{amount:0,
+			name:"Wooden Tools",
+			cost:0,
+			boost:["human",0.1]
+		},
+	"stonetools":{amount:0,
+			name:"Stone Tools",
+			cost:0,
+			boost:["human",0.1]
+	},"ironres":{amount:0,
+		name:"Iron Research Facillity",
+		cost:0,
+		boost:["human",0.1],
+	},"hitech":{amount:0,
+		name:"Hi Tech",
+		cost:0,
+		boost:["human",0.1]
+	},
+	// boosts all
+	"diode":{amount:0,
+			name:"Diode",
+			cost:0,
+			boost:["all",0.1]
+		},
+	"factweb":{amount:0,
+			name:"Factory Website",
+			cost:0,
+			boost:["all",0.1]
+		},
+	"tvads":{amount:0,
+			name:"TV advertisement",
+			cost:0,
+			boost:["all",0.1]
+		},
+	"redditbot":{amount:0,
+			name:"Reddit BotBulb",
+			cost:0,
+			boost:["all",0.1]
+		},
+	"bulbcon":{amount:0,
+			name:"Bulb Convention",
+			cost:0,
+			boost:["all",0.1]
+		},
+	"warzone":{amount:0,
+			name:"Lightbulb Warzone",
+			cost:0,
+			boost:["all",0.1]
+		},
+};
