157dededa21d138ef26d731b4126b707785c3432
idunnowhy9000 <idunnowhy9000@gmail.com>
2014-07-11 20:16:34 +0700

    ...
    Added achievements

--- js/script.js
+++ js/script.js
@@ -6,9 +6,6 @@ var
 		amountBulb=[],
 		amountUpgrade=[],
 		clickBoost = 1,
-		playerSet={
-			"firstTime": 0
-		},
 		items=[];
 function initGame(){
 	// print buildings
@@ -29,12 +26,13 @@ function initGame(){
 	for (b in upgrades){
 		function bU(up){
 			return function(){
+				console.log(up);
 				buy_upgrade(up);
 				updateTable();
 			}
 		}
 		$("#upgrade").append("<div class='upgradesBox' id='upgrades-"+b+"'><img src='img/upgrades/"+b+".png'></div>");
-		$("#" + b).click(bU(b))
+		$("#upgrades-" + b).click(bU(b))
 	};
 	// earn volts per click
 	$("#bulb").click(function(){earnVolt(1);});
@@ -59,6 +57,15 @@ cost = calcCost(x);
 		x.amount++;
 		volt-=cost;
 		updateTable();
+		// achievements
+		am = x.amount;
+		n = x.name.split(" ")[0];
+		console.log(am,x,n);
+		// incandescent
+		if(n == "Incandescent" && am == 1) earnAchievement("edisonstep");
+		if(n == "Incandescent" && am == 2) earnAchievement("double");
+		if(n == "Incandescent" && am == 50) earnAchievement("fiftyshades");
+		if(n == "Incandescent" && am == 100) earnAchievement("hundredfoot");
 	}
 }
 function buy_upgrade(x){
@@ -71,15 +78,25 @@ console.log(i);
 		volt -= i.cost;
 		updateTable();
 		console.log("bought item");
-		$("#" + x).remove();
+		$("#upgrades-" + x).remove();
 	}
 }
 function earnVolt(i){
 	volt+=i;
 	volttot+=i;
 	updateSidebar();
+	// check volt for achievement
+	function checkV(v){return (volt > v) ? true : false;}
+	if(checkV(1)) earnAchievement("firsttime");
+	if(checkV(100)) earnAchievement("hundreds");
+	if(checkV(1000)) earnAchievement("thoudsands");
+	if(checkV(1000000)) earnAchievement("millions");
+	if(checkV(10000000)) earnAchievement("tenmillion");
 }
 function updateTable(){
+// clear everything
+
+//
 	for(b in lightbulb){
 		$("#" + b + " .buildingAmount").text(lightbulb[b].amount);
 		$("#" + b + " .buildingCost").text(calcCost(lightbulb[b]));
@@ -93,7 +110,7 @@ function updateTable(){
 		if(uG.amount == 1){continue;}
 		else{
 			$("#upgrades-" + u).tooltipster({
-				content:uG.name + "<br>"+((typeof uG.desc == "undefined") ? "..." :uG.desc)+" <br>Costs "+numberWithCommas(uG.cost)+" volts.",
+				content:uG.name + "<br><span class='upgradeDesc'>"+((typeof uG.desc == "undefined") ? "..." :uG.desc)+"</span><br><span class='upgradeCost'>Costs "+numberWithCommas(uG.cost)+" volts.</span>",
 				contentAsHTML:true
 			});
 		}
@@ -102,6 +119,17 @@ function updateTable(){
 function updateSidebar(i){
 	$("#count").text(numberWithCommas(Math.round(volt)) + " volts");
 	if(!(typeof i ==='undefined'))$("#vps").text(i.toFixed(2) + " volts/second");
+}
+function showAchievement(name){
+	$("#achievements").hide().append('<div class="achievement"><div class=achievementImg></div><div class="achievementInfo"><div class="achievementNote">Achievement Unlocked!</div><div class=achievementName>'+name+'</div></div></div>').fadeIn().delay(1000).fadeOut();
+}
+function earnAchievement(id){
+	var thing = achievements[id];
+	if(thing.amount == 0){
+		showAchievement(thing.name);
+		thing.amount = 1;
+		console.log("bought");
+	}
 }
 ////
 $(document).ready(function(){
--- js/upgradedata.js
+++ js/upgradedata.js
@@ -1,128 +1,160 @@
 var upgrades = {
-	// incandescent bulbs
-	"incandescentcy":{
-			amount:0,
-			name:"Thomas Edison",
-			cost:0,
-			boost:["incandescent",0.1]
-		},
-	"blueprints":{amount:0,
-			name:"Blueprints!",
-			cost:0,
-			boost:["incandescent",0.1]
-		},
-	"inventor":{
-		amount:0,
-		name:"Albert Einstein Clone",
-		cost:0,
-		desc:"Hire a clone and he'll (probably) help you out!",
-		boost:["incandescent",0.1]
-	},
-	// bulb smasher
-	"experimentroom":{amount:0,
-			name:"Experimentation Room",
-			cost:0,
-			boost:["smasher",0.1]
-		},
-	"bigham":{ // it's not what it sound like
-			name:"Big Hammer",
-			cost:0,
-			boost:["smasher",0.1],
-			amount:0
-		},
-	// thundercollector
-	"relaxation":{amount:0,
-			name:"Electron Relaxation",
-			cost:0,
-			boost:["thundercol",0.1],
-		},
-	"excitation":{amount:0,
-			name:"Electron Excitation",
-			desc:"The ultimate Electric Party!",
-			cost:0,
-			boost:["thundercol",0.1],
-		},
-	// bacterial
-	"fungus":{amount:0,
-			name:"Fungus",
-			desc:"I have to say, I'm a fungi!",
-			cost:0,
-			boost:["bacterial",0.1]
-		},
-	// dinosaur
-	"megadinos":{amount:0,
-			name:"Mega DinoBulbs",
-			cost:0,
-			boost:["dinosaur",0.1],
-			desc:"Still better than keys"
-		},
-	// human
-	"evo":{amount:0,
-			name:"Lightbulb Charles Darwin",
-			cost:0,
-			boost:["human",0.1],
-			desc:"Fun fact: The Greeks actually proposed the theory before Charles Darwin"
-		},
-	"creation":{amount:0,
-			name:"Worship",
-			cost:0,
-			boost:["human",0.1],
-			desc:"Somehow human lightbulbs also worship a \"Supreme Being\", coincident?"
-		},
-	"woodtools":{amount:0,
-			name:"Wooden Tools",
-			cost:0,
-			boost:["human",0.1]
-		},
-	"stonetools":{amount:0,
-			name:"Stone Tools",
-			cost:0,
-			boost:["human",0.1]
-	},
-	"irontools":{
-	amount:0,
-	name:"Iron Tools",
-	cost:0,
-	boost:["human",0.1]
-	},"ironres":{amount:0,
-		name:"Iron Research Facillity",
-		cost:0,
-		boost:["human",0.1],
-	},"hitech":{amount:0,
-		name:"Hi Tech",
-		cost:0,
-		boost:["human",0.1]
-	},
-	// boosts all
-	"diode":{amount:0,
-			name:"Diode",
-			cost:0,
-			boost:["all",0.1]
-		},
-	"fact":{amount:0,
-			name:"Factories",
-			cost:0,
-			boost:["all",0.1]
-		},
-	"tvads":{amount:0,
-			name:"ShamWow Guy",
-			cost:0,
-			desc:"Hires the ShamWow Guy to advertise lightbulbs",
-			boost:["all",0.1]
-		},
-	"redditbot":{amount:0,
-			name:"Reddit BotBulb",
-			cost:0,
-			boost:["all",0.1]
-		},
-	"bulbcon":{amount:0,
-			name:"Bulb Convention",
-			cost:0,
-			boost:["all",0.1]
-		},
-	"warzone":{amount:0,
-			name:"Lightbulb Warzone",
-			cost:0,
-			boost:["all",0.1]
-		},
+    // incandescent bulbs
+    "incandescentcy": {
+        amount: 0,
+        name: "Thomas Edison",
+        cost: 0,
+        boost: ["incandescent", 0.1]
+    },
+    "joseph": {
+        amount: 0,
+        name: "Joseph Swan",
+        cost: 0,
+        boost: ["incandescent", 0.1]
+    },
+    "hiram": {
+        amount: 0,
+        name: "Hiram Maxim",
+        cost: 0,
+        boost: ["incandescent", 0.1]
+    },
+    "blueprints": {
+        amount: 0,
+        name: "Blueprints!",
+        cost: 0,
+        boost: ["incandescent", 0.1]
+    },
+    "inventor": {
+        amount: 0,
+        name: "Albert Einstein Clone",
+        cost: 0,
+        desc: "Hire a clone and he'll (probably) help you out!",
+        boost: ["incandescent", 0.1]
+    },
+    // bulb smasher
+    "experimentroom": {
+        amount: 0,
+        name: "Experimentation Room",
+        cost: 0,
+        boost: ["smasher", 0.1]
+    },
+    "bigham": { // it's not what it sound like
+        name: "Big Hammer",
+        cost: 0,
+        boost: ["smasher", 0.1],
+        amount: 0
+    },
+    // thundercollector
+    "relaxation": {
+        amount: 0,
+        name: "Electron Relaxation",
+        cost: 0,
+        boost: ["thundercol", 0.1],
+    },
+    "excitation": {
+        amount: 0,
+        name: "Electron Excitation",
+        desc: "The ultimate Electric Party!",
+        cost: 0,
+        boost: ["thundercol", 0.1],
+    },
+    // bacterial
+    "fungus": {
+        amount: 0,
+        name: "Fungus",
+        desc: "I have to say, I'm a fungi!",
+        cost: 0,
+        boost: ["bacterial", 0.1]
+    },
+    // dinosaur
+    "megadinos": {
+        amount: 0,
+        name: "Mega DinoBulbs",
+        cost: 0,
+        boost: ["dinosaur", 0.1],
+        desc: "Still better than keys"
+    },
+    // human
+    "evo": {
+        amount: 0,
+        name: "Lightbulb Charles Darwin",
+        cost: 0,
+        boost: ["human", 0.1],
+        desc: "Fun fact: The Greeks actually proposed the theory before Charles Darwin"
+    },
+    "creation": {
+        amount: 0,
+        name: "Worship",
+        cost: 0,
+        boost: ["human", 0.1],
+        desc: "Somehow human lightbulbs also worship a \"Supreme Being\", coincident?"
+    },
+    "woodtools": {
+        amount: 0,
+        name: "Wooden Tools",
+        cost: 0,
+        boost: ["human", 0.1]
+    },
+    "stonetools": {
+        amount: 0,
+        name: "Stone Tools",
+        cost: 0,
+        boost: ["human", 0.1]
+    },
+    "irontools": {
+        amount: 0,
+        name: "Iron Tools",
+        cost: 0,
+        boost: ["human", 0.1]
+    },
+    "goldtools": {
+        amount: 0,
+        name: "Gold Tools",
+        cost: 0,
+        boost: ["human", 0.1],
+    },
+    "hitech": {
+        amount: 0,
+        name: "Hi Tech",
+        cost: 0,
+        boost: ["human", 0.1]
+    },
+    // boosts all
+    "diode": {
+        amount: 0,
+        name: "Diode",
+        cost: 0,
+        boost: ["all", 0.1]
+    },
+    "fact": {
+        amount: 0,
+        name: "Factories",
+        cost: 0,
+        boost: ["all", 0.1]
+    },
+    "tvads": {
+        amount: 0,
+        name: "ShamWow Guy",
+        cost: 0,
+        desc: "Hires the ShamWow Guy to advertise lightbulbs",
+        boost: ["all", 0.1]
+    },
+    "redditbot": {
+        amount: 0,
+        name: "Reddit BotBulb",
+        cost: 0,
+        boost: ["all", 0.1]
+    },
+    "bulbcon": {
+        amount: 0,
+        name: "Bulb Convention",
+        cost: 0,
+        boost: ["all", 0.1]
+    },
+    "warzone": {
+        amount: 0,
+        name: "Lightbulb Warzone",
+        cost: 0,
+        boost: ["all", 0.1]
+    },
 };
