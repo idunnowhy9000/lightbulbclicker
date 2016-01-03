d1cfab49255e089ae2ae9ce46ddb2ce021e6b6a9
idunnowhy9000 <idunnowhy9000@gmail.com>
2016-01-03 20:53:46 +0700

    Compiled

---
+++ build.js
@@ -0,0 +1,36 @@
+({
+	baseUrl: 'js/',
+	paths: {
+		jquery: 'lib/jquery',
+		bootstrap: 'lib/bootstrap.min',
+		tooltip: 'lib/tooltip',
+		backbone: 'lib/backbone',
+		underscore: 'lib/underscore',
+		bootbox: 'lib/bootbox.min',
+		text: 'lib/text',
+		requirejs: 'lib/require'
+	},
+	shim: {
+		jquery: { exports: '$' },
+		underscore: { exports: '_' },
+		backbone: {
+			deps: ['jquery', 'underscore'],
+			exports: 'Backbone'
+		},
+		localStorage: {
+			deps: ['backbone']
+		},
+		bootstrap: {
+			deps: ['jquery']
+		},
+		bootbox: {
+			deps: ['jquery', 'bootstrap']
+		},
+		tooltip: {
+			deps: ['jquery', 'bootstrap']
+		}
+	},
+	name: 'main',
+	out: 'js/script.js',
+	include: ['requirejs']
+})
