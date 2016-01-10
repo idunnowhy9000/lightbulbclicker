e865c7f7c675c54e6b3babf2925fc24c21009d29
idunnowhy9000 <idunnowhy9000@gmail.com>
2016-01-10 21:04:11 +0700

    remove this backbone

--- build.js
+++
@@ -1,7 +0,0 @@
-({
-	//optimize: 'none',
-	baseUrl: 'js/',
-	name: 'main',
-	out: 'js/script.js',
-	mainConfigFile: 'js/main.js'
-})
--- js/data/weather.data.js
+++
@@ -1,9 +0,0 @@
-(function (Game) {
-	"use strict";
-	window.Game.WeatherD = [
-		{
-			name: "Sunny Day",
-			
-		}
-	];
-})(window.Game || {});
